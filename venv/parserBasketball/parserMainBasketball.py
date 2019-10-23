87c795ae5b2f3676eeb6d0d82e286d94fc691109
stetdim1 <55704422+stetdim1@users.noreply.github.com>
2019-10-23 22:19:49 +0300

    final version of parser Basketball results

--- venv/parserBasketball/parserMainBasketball.py
+++ venv/parserBasketball/parserMainBasketball.py
@@ -130,24 +130,42 @@ def parseSubTitleBasketballRounds(content):
         if checkAOT == keyWordAOT:
             aot = 1
 
-
-
         codeEventString = content[positionOfEventKeyWord + len(eventKeyWord): positionOfEventKeyWord + len(eventKeyWord) + 8]
 
+        homeTeamPass = ''
+        awayTeamPass = ''
 
-        # checkWinner
+
         # fontBold"Toronto Raptors<div class="event__icon icon - -winner">
 
         homeTeam = content[content.find(eventKeyWordHomeTeam, positionOfEventKeyWord) + len(eventKeyWordHomeTeam) :
                            content.find('</div>', content.find(eventKeyWordHomeTeam, positionOfEventKeyWord))]
         homeTeam = homeTeam.replace(' fontBold">', '')
         homeTeam = homeTeam.replace('">', '')
+
+        #Begin recognise a team which pass a round
+        homePass = content[content.find('</div>', content.find(eventKeyWordHomeTeam, positionOfEventKeyWord)) - 38 :
+                           content.find('</div>', content.find(eventKeyWordHomeTeam, positionOfEventKeyWord))]
+        awayPass = content[content.find('</div>', content.find(eventKeyWordAwayTeam, positionOfEventKeyWord)) - 38:
+                           content.find('</div>', content.find(eventKeyWordAwayTeam, positionOfEventKeyWord))]
+
+        if  homePass == '<div class="event__icon icon--winner">':
+            homeTeamPass = 1
+            awayTeamPass = 0
+
+        if awayPass == '<div class="event__icon icon--winner">':
+            homeTeamPass = 0
+            awayTeamPass = 1
+
+        #End recognise a team which pass a round
+
         homeTeam = homeTeam.replace('<div class="event__icon icon--winner', '')
 
         awayTeam = content[content.find(eventKeyWordAwayTeam, positionOfEventKeyWord) + len(eventKeyWordAwayTeam):
                            content.find('</div>', content.find(eventKeyWordAwayTeam, positionOfEventKeyWord))]
         awayTeam = awayTeam.replace(' fontBold">', '')
         awayTeam = awayTeam.replace('">', '')
+
         awayTeam = awayTeam.replace('<div class="event__icon icon--winner', '')
 
 
@@ -165,7 +183,7 @@ def parseSubTitleBasketballRounds(content):
                                           str(scoreParser(eventKeyWordHomeScore_Second_Quoter, positionOfEventKeyWord)) + ' - ' +
                                           str(scoreParser(eventKeyWordHomeScore_Third_Quoter, positionOfEventKeyWord)) + ' - ' +
                                           str(scoreParser(eventKeyWordHomeScore_Fourth_Quoter, positionOfEventKeyWord)) +
-                                          aotScoreHome
+                                          aotScoreHome + ' ' + str(homeTeamPass)
                                           ))
 
         print(str(awayTeam) + ' - ' + str(scoreParser(eventKeyWordAwayScore_Total, positionOfEventKeyWord) + ' - ' +
@@ -173,125 +191,20 @@ def parseSubTitleBasketballRounds(content):
                                           str(scoreParser(eventKeyWordAwayScore_Second_Quoter, positionOfEventKeyWord)) + ' - ' +
                                           str(scoreParser(eventKeyWordAwayScore_Third_Quoter, positionOfEventKeyWord)) + ' - ' +
                                           str(scoreParser(eventKeyWordAwayScore_Fourth_Quoter, positionOfEventKeyWord)) +
-                                          aotScoreAway
+                                          aotScoreAway + ' ' + str(awayTeamPass)
                                           ))
 
         positionOfEventKeyWord = content.find(eventKeyWord, positionOfEventKeyWord + len(eventKeyWord))
         i += 1
 
 
-
-
-
-
-
-def parseRound(incomeStringRound):
-#define the round
-    criteriaFirstRound = '<div class="event__round event__round--static">'
-    criteriaSecondRound = '</div>'
-    beginOftheStringRound = incomeStringRound.find(criteriaFirstRound)
-    endOftheStringRound = incomeStringRound.find(criteriaSecondRound)
-    round = incomeStringRound[beginOftheStringRound + len(criteriaFirstRound):endOftheStringRound]
-    return round
-
-def parseCodeEventString(incomeCodeEventString):
-    # define the code event string
-    criteriaFirstCodeEventString = 'g_1_'
-    criteriaSecondCodeEventString = 'Click for match detail!'
-    countIncomeEventString = incomeCodeEventString.count(criteriaFirstCodeEventString)
-    j = 0
-    i = 1
-    while i <=countIncomeEventString:
-        beginOfTheCodeEventString = incomeCodeEventString.find(criteriaFirstCodeEventString, j)
-        j = beginOfTheCodeEventString + 1
-        endOfTheCodeEventString = incomeCodeEventString.find(criteriaSecondCodeEventString, j)
-        outcomeCodeEventString = incomeCodeEventString[beginOfTheCodeEventString+len(criteriaFirstCodeEventString):]
-        codeEventString = incomeCodeEventString[beginOfTheCodeEventString + len(criteriaFirstCodeEventString): endOfTheCodeEventString-9]
-        t = str(parseTimeEventString(outcomeCodeEventString))
-        h = str(parseEventHomeTeamString(outcomeCodeEventString))
-        hos = str(parseEventHomeScoreString(outcomeCodeEventString))
-        aws = str(parseEventAwayScoreString(outcomeCodeEventString))
-        aw = str(parseEventAwayTeamString(outcomeCodeEventString))
-
-        print(codeEventString + ' ' + t + ' ' + h + ' ' + hos + ' - ' + aws + ' ' + aw)
-        i+=1
-
-def parseTimeEventString(incomeTimeEventString):
-    # define the time
-    criteriaFirstTime = 'event__time'
-    beginOfTheTimeEventString = incomeTimeEventString.find(criteriaFirstTime)
-    timeEventString = incomeTimeEventString[beginOfTheTimeEventString + 13:beginOfTheTimeEventString+25]
-    return timeEventString
-
-def parseEventHomeTeamString(incomeEventHomeTeamString):
-    # define home team
-    criteriaFirstEventHomeTeamString = 'event__participant event__participant--home'
-    criteriaSecondEventHomeTeamString = '</div>'
-    incomeEventHomeTeamString = incomeEventHomeTeamString.replace('<div class ="icon icon--redCard redCard--first">', '')
-    incomeEventHomeTeamString = incomeEventHomeTeamString.replace('<div class="icon icon--redCard redCard--first redCard--last">', '')
-    beginOfTheEventHomeTeamString = incomeEventHomeTeamString.find(criteriaFirstEventHomeTeamString)
-    endOfTheEventHomeTeamString = incomeEventHomeTeamString.find(criteriaSecondEventHomeTeamString, beginOfTheEventHomeTeamString)
-    eventHomeTeamStringPre = incomeEventHomeTeamString[beginOfTheEventHomeTeamString + 45:endOfTheEventHomeTeamString]
-    eventHomeTeamString = eventHomeTeamStringPre.replace('ontBold">', '')
-    return eventHomeTeamString
-
-
-def parseEventAwayTeamString(incomeEventAwayTeamString):
-    # define away team
-    criteriaFirstEventAwayTeamString = 'event__participant event__participant--away'
-    criteriaSecondEventAwayTeamString = '</div>'
-    incomeEventAwayTeamString = incomeEventAwayTeamString.replace('<div class ="icon icon--redCard redCard--first">', '')
-    incomeEventAwayTeamString = incomeEventAwayTeamString.replace('<div class="icon icon--redCard redCard--first redCard--last">', '')
-    beginOfTheEventAwayTeamString = incomeEventAwayTeamString.find(criteriaFirstEventAwayTeamString)
-    endOfTheEventAwayTeamString = incomeEventAwayTeamString.find(criteriaSecondEventAwayTeamString, beginOfTheEventAwayTeamString)
-    eventAwayTeamStringPre = incomeEventAwayTeamString[beginOfTheEventAwayTeamString + 45:endOfTheEventAwayTeamString]
-    eventAwayTeamString = eventAwayTeamStringPre.replace('ontBold">', '')
-    # print(eventAwayTeamString)
-    return eventAwayTeamString
-
-def parseEventHomeScoreString(incomeEventHomeScoreString):
-    #define
-    criteriaFirstEventHomeScoreString = '<span>'
-    criteriaSecondEventHomeScoreString = '</span>'
-    beginOfTheEventHomeScoreString = incomeEventHomeScoreString.find(criteriaFirstEventHomeScoreString)
-    endOfTheEventHomeTeamString = incomeEventHomeScoreString.find(criteriaSecondEventHomeScoreString, beginOfTheEventHomeScoreString)
-    eventHomeScoreString = incomeEventHomeScoreString[beginOfTheEventHomeScoreString+len(criteriaFirstEventHomeScoreString):endOfTheEventHomeTeamString]
-    return eventHomeScoreString
-
-def parseEventAwayScoreString(incomeEventAwayScoreString):
-    criteriaFirstEventAwayScoreString = 'nbsp;<span>'
-    criteriaSecondEventAwayScoreString = '</span>'
-    beginOfTheEventAwayScoreString = incomeEventAwayScoreString.find(criteriaFirstEventAwayScoreString)
-    endOfTheEventAwayTeamString = incomeEventAwayScoreString.find(criteriaSecondEventAwayScoreString, beginOfTheEventAwayScoreString)
-    eventAwayScoreString = incomeEventAwayScoreString[beginOfTheEventAwayScoreString + len(criteriaFirstEventAwayScoreString):endOfTheEventAwayTeamString]
-    # print(eventAwayScoreString)
-    return eventAwayScoreString
-
 if __name__=='__main__':
     path = r'Archive/NBA 2018_2019 Results - Basketball_USA.html'
     # firstCriteria = '<div class="event__round event__round--static">'
     file = open(path)
     contentGlobal = file.read()
-    # parseSeasonBasketball(contentGlobal)
+    parseSeasonBasketball(contentGlobal)
     parseTitleBasketball(contentGlobal)
 
     print(globalAOTCount)
     print(globalMatchCount)
-    # print(contentGlobal[97195:182117])
-    # print(contentGlobal[182117:540225])
-    # print(contentGlobal[540225:542785])
-    # print(contentGlobal[542785:1393398])
-    # print(contentGlobal[1393398:-1])
-    # print(contentGlobal[97195+17090:97195+17090+120])
-    # print(contentGlobal[97195+17090:97195+17090+120])
-    # print(contentGlobal[1393398 : -1])
-    # i = 1
-    # j = 0
-    # while i <= countFirstCriteria:
-    #     beginOftheString = content.find(firstCriteria, j)
-    #     j = beginOftheString + 1
-    #     endOfTheString = content.find(firstCriteria, j)
-    #     outcomeString = content[beginOftheString:endOfTheString]
-    #     print(parseRound(outcomeString))
-    #     # parseCodeEventString(outcomeString)
-    #     i += 1
